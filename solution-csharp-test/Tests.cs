0b3d3f55723f2ac30714f07d263b8f148f39dceb
Jonathan Burke <jonathan.burke.1311@gmail.com>
2015-10-07 20:11:20 +0200

    Initial commit * naive A* implementation, no concurrency, no optimization

---
+++ solution-csharp-test/Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using solution_csharp;
+using System.Linq;
+
+namespace solution_csharp_test
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestExample1()
+        {
+            var pMap = new char[] { (char)1, (char)1, (char)1, (char)1, (char)0, (char)1, (char)0, (char)1, (char)0, (char)1, (char)1, (char)1 };
+            var pOutBuffer = new int[12];
+            var result = Solution.FindPath(0, 0, 1, 2, pMap, 4, 3, pOutBuffer, 12);
+
+            Assert.AreEqual(3, result);
+            Assert.AreEqual(1, pOutBuffer[0]);
+            Assert.AreEqual(5, pOutBuffer[1]);
+            Assert.AreEqual(9, pOutBuffer[2]);
+        }
+
+        [TestMethod]
+        public void TestExample2()
+        {
+            var pMap = new char[] { (char)0, (char)0, (char)1, (char)0, (char)1, (char)1, (char)1, (char)0, (char)1 };
+            var pOutBuffer = new int[7];
+            var result = Solution.FindPath(2, 0, 0, 2, pMap, 3, 3, pOutBuffer, 7);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestCustom1()
+        {
+            var pMap = Enumerable.Range(0, 225).Select(i => (char)1).ToArray();
+            var pOutBuffer = new int[200];
+            var result = Solution.FindPath(0, 0, 14, 14, pMap, 15, 15, pOutBuffer, 200);
+
+            Assert.AreEqual(28, result);
+        }
+
+        [TestMethod]
+        public void TestCustom2()
+        {
+            var pMap = Enumerable.Range(0, 1000000).Select(i => (char)1).ToArray();
+            var pOutBuffer = new int[1000000];
+            var result = Solution.FindPath(0, 0, 999, 999, pMap, 1000, 1000, pOutBuffer, 1000000);
+
+            Assert.AreEqual(1998, result);
+        }
+    }
+}
---
+++ solution-csharp/Solution.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solution_csharp
+{
+    public class Solution
+    {
+        class WorkItem
+        {
+            public int Index { get; set; }
+
+            public int Weight { get; set; }
+
+            public WorkItem Predecessor { get; set; }
+
+            public bool IsExpanded { get; set; }
+        }
+
+        class WorkItemComparer : IComparer<double>
+        {
+            public int Compare(double x, double y)
+            {
+                return y > x ? 1 : -1;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nStartX">0-based coordinates of the start position</param>
+        /// <param name="nStartY">0-based coordinates of the start position</param>
+        /// <param name="nTargetX">0-based coordinates of the target position</param>
+        /// <param name="nTargetY">0-based coordinates of the target position</param>
+        /// <param name="pMap">describes a grid of width nMapWidth and height nMapHeight.
+        /// The grid is given in row-major order, each row is given in order of increasing x-coordinate,
+        /// and the rows are given in order of increasing y-coordinate. Traversable locations of the grid
+        /// are indicated by 1, and impassable locations are indicated by 0. Locations are considered
+        /// to be adjacent horizontally and vertically but not diagonally</param>
+        /// <param name="nMapWidth"></param>
+        /// <param name="nMapHeight"></param>
+        /// <param name="pOutBuffer">is where you should store the positions visited in the found path,
+        /// excluding the starting position but including the final position. Entries in pOutBuffer
+        /// are indices into pMap</param>
+        /// <param name="nOutBufferSize">is the maximum amount number of entries that can be written to pOutBuffer</param>
+        /// <returns>the length of the shortest path between Start and Target, or −1 if no such path exists</returns>
+        public static int FindPath(int nStartX, int nStartY, int nTargetX, int nTargetY, char[] pMap, int nMapWidth, int nMapHeight, int[] pOutBuffer, int nOutBufferSize)
+        {
+            if (nOutBufferSize == 0)
+                return -1;
+
+            var mapWeight = new WorkItem[pMap.Length];
+
+            var sPos = nStartX + nStartY * nMapWidth;
+            var tPos = nTargetX + nTargetY * nMapWidth;
+
+            Action<int, WorkItem, ConcurrentPriorityQueue.ConcurrentPriorityQueue<WorkItem, double>, Func<int, double>> addItem = (index, predecessor, queue, heuristic) =>
+            {
+                if (index >= 0 && index < mapWeight.Length)
+                {
+                    var newWeight = predecessor.Weight + 1;
+                    var distance = heuristic(index);
+                    var item = mapWeight[index] ?? (mapWeight[index] = new WorkItem { Index = index });
+                    if (item.IsExpanded)
+                        return;
+                    if (!queue.Contains(item))
+                        queue.Enqueue(item, newWeight + distance);
+                    else if (newWeight < item.Weight)
+                        queue.UpdatePriority(item, newWeight + distance);
+                    item.Weight = newWeight;
+                    item.Predecessor = predecessor;
+                }
+            };
+
+            Action<WorkItem, ConcurrentPriorityQueue.ConcurrentPriorityQueue<WorkItem, double>, Func<int, double>> expandItem = (item, queue, heuristic) =>
+            {
+                item.IsExpanded = true;
+                var edge = item.Index % nMapWidth;
+                if (edge > 0)
+                    addItem(item.Index - 1, item, queue, heuristic);
+                if (edge < nMapWidth - 1)
+                    addItem(item.Index + 1, item, queue, heuristic);
+                addItem(item.Index - nMapWidth, item, queue, heuristic);
+                addItem(item.Index + nMapWidth, item, queue, heuristic);
+            };
+
+            Action floodWeight = () =>
+            {
+                var queue = new ConcurrentPriorityQueue.ConcurrentPriorityQueue<WorkItem, double>(new WorkItemComparer());
+                queue.Enqueue(new WorkItem { Index = sPos, Weight = 1 }, 1);
+
+                Func<int, double> heuristic = (index) => Math.Abs((double)nTargetX - index % nMapWidth) + Math.Abs((double)nTargetY - index / nMapWidth);
+
+                while (queue.Count > 0)
+                {
+                    var next = queue.Dequeue();
+
+                    if (next.Index == tPos)
+                        return;
+
+                    if (pMap[next.Index] == 1)
+                    {
+                        expandItem(next, queue, heuristic);
+                    }
+                }
+            };
+
+            floodWeight();
+
+            Func<WorkItem, int, int> walk = null;
+            walk = (item, depth) =>
+            {
+                if (item == null && depth == 0)
+                    return -1;
+                if (item.Predecessor == null)
+                    return 0;
+
+                var max = walk(item.Predecessor, depth + 1);
+                pOutBuffer[max] = item.Index;
+                return max + 1;
+            };
+
+            return walk(mapWeight[tPos], 0);
+        }
+    }
+}
